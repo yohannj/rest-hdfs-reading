8b819f560a3c4c8eb525b2da57375811022e7420
Yohann Jardin <yjardin@smartadserver.com>
2018-01-10 21:14:22 +0100

    Added ignite install script

--- scripts/install_ignite.sh
+++ scripts/install_ignite.sh
@@ -1,40 +1,77 @@
 #!/bin/bash
 
-if [ ! 	-d "$HADOOP_HOME" ]; then
-	echo "Hadoop must first be installed. Installation will not pursue."
+usage()
+{
+    echo "Install Apache Ignite 2.3.0"
+    echo ""
+    echo "$0"
+    echo -e "\t-hdfsmaster IP or name and port of the HDFS master node, example: -hdfsmaster=localhost:9000"
+    echo ""
+}
+
+while [ "$1" != "" ]; do
+    PARAM=`echo $1 | awk -F= '{print $1}'`
+    VALUE=`echo $1 | awk -F= '{print $2}'`
+    case $PARAM in
+        -h | --help)
+            usage
+            exit
+            ;;
+        -hdfsmaster)
+            HDFS_MASTER=$VALUE
+            ;;
+        *)
+            echo "ERROR: unknown parameter \"$PARAM\""
+            usage
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+if [ -z ${HDFS_MASTER} ]; then
+	usage
 	exit -1;
 fi
 
-if [ -d "apache-ignite-2.3.0-src" ]; then
-	echo "Found folder apache-ignite-2.3.0/. Ignite seems to have already been installed. Installation will not pursue."
+if [ ! -d "$HADOOP_HOME" ]; then
+	echo "HADOOP_HOME environment variable not set. Installation will not pursue."
 	exit -1;
 fi
 
-if [ ! -f "apache-ignite-2.3.0-src.zip" ]; then
+if [ -d "apache-ignite-hadoop-2.3.0-bin" ]; then
+	echo "Found folder apache-ignite-hadoop-2.3.0-bin/. Ignite seems to have already been installed. Installation will not pursue."
+	exit -1;
+fi
+
+if [ ! -f "apache-ignite-hadoop-2.3.0-bin.zip" ]; then
 	echo "Downloading apache ignite version 2.3.0 sources"
-	curl -O http://apache.mediamirrors.org//ignite/2.3.0/apache-ignite-2.3.0-src.zip
+	curl -O http://apache.mediamirrors.org//ignite/2.3.0/apache-ignite-hadoop-2.3.0-bin.zip
 	echo "Download finished"
 else
 	echo "Sources of ignite version 2.3.0 has already been downloaded"
 fi
 
 echo "Extracting archive"
-unzip -q apache-ignite-2.3.0-src.zip
+unzip -q apache-ignite-hadoop-2.3.0-bin.zip
 echo "Extraction done"
-rm apache-ignite-2.3.0-src.zip
+rm apache-ignite-hadoop-2.3.0-bin.zip
 echo "Archive deleted"
 
-# Following lines were found on https://ignite.apache.org/download.cgi#sources, part Building the Binaries
-cd apache-ignite-2.3.0-src
-mvn clean package -DskipTests -Dignite.edition=hadoop -Dhadoop.version=3.0.0
-cd ..
-
-ln -sfn apache-ignite-2.3.0-src apache-ignite
+ln -sfn apache-ignite-hadoop-2.3.0-bin apache-ignite
 echo "Symbolic link apache-ignite created/updated to version 2.3.0"
 
 # Seems like ignite is not looking for hadoop jars at the right place
 ln -sfn $HADOOP_HOME/share/hadoop/mapreduce $HADOOP_HOME/share/hadoop/mapreduce/lib
-echo "Symbolic link from /home/hadoop/hadoop/share/hadoop/mapreduce/lib to /home/hadoop/hadoop/share/hadoop/mapreduce created/updated"
+echo "Symbolic link from $HADOOP_HOME/share/hadoop/mapreduce/lib to $HADOOP_HOME/share/hadoop/mapreduce created/updated"
+
+ln -sfn "$(pwd)/apache-ignite/libs/ignite-core-2.3.0.jar" $HADOOP_HOME/share/hadoop/common/lib/ignite-core-2.3.0.jar
+ln -sfn "$(pwd)/apache-ignite/libs/ignite-shmem-1.0.0.jar" $HADOOP_HOME/share/hadoop/common/lib/ignite-shmem-1.0.0.jar
+ln -sfn "$(pwd)/apache-ignite/libs/ignite-hadoop/ignite-hadoop-2.3.0.jar" $HADOOP_HOME/share/hadoop/common/lib/ignite-hadoop-2.3.0.jar
+
+perl -0777 -i -pe 's/\n.*?<!--(\n.*?<property name="secondaryFileSystem">\n(?:.*\n)+?).*-->\n/$1/g' apache-ignite/config/default-config.xml
+perl -i -pe "s/your_hdfs_host:9000/${HDFS_MASTER}\//g" apache-ignite/config/default-config.xml
+echo "apache-ignite/config/default-config.xml updated"
 
 echo "Installation successful"
 exit 0;
