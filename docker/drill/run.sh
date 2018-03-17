6ee372bd6ff67d12fd851311d449a0e0dd22fec3
Yohann Jardin <yohann.jardin@telecomnancy.net>
2018-03-17 12:33:18 +0100

    Base Drill image on alpine

--- docker/drill/run.sh
+++ docker/drill/run.sh
@@ -1,10 +1,8 @@
-#!/bin/bash
+#!/bin/ash
 
-/etc/init.d/ssh start
-
-sleep 20
+sleep 20 # Wait for zookeeper cluster to start
 $DRILL_HOME/bin/drillbit.sh start
-sleep 20
+sleep 20 # Wait for drill to start
 curl -X POST -H "Content-Type: application/json" -d '{"name":"hdfs", "config": {"type": "file", "enabled": true, "connection": "hdfs://namenode:9000/", "workspaces": { "root": { "location": "/", "writable": false, "defaultInputFormat": null}}, "formats": null}}' http://drill:8047/storage/hdfs.json
 
 tail -f /dev/null
